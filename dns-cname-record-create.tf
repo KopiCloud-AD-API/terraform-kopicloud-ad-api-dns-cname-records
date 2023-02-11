68bbaefa510f0b8ccc32c282fcd1e738b3610806
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-11 01:36:18 +0100

    Initial commit

---
+++ dns-cname-record-create.tf
@@ -0,0 +1,16 @@
+#################################################
+## KopiCloud AD API - Create DNS CNAME Records ##
+#################################################
+
+# Create a DNS CNAME Record for a machine
+resource "kopicloud_dns_cname_record" "test_cname" {
+  hostname       = "tftestcn01"
+  hostname_alias = "tftestcn01_alias"
+  zone_name      = "kopicloud.local"
+}
+
+# Output Created DNS CNAME Record 
+output "dns_cname_record" {
+  description = "Created DNS CNAME Record"
+  value       = resource.kopicloud_dns_cname_record.test_cname
+}
