51366a1d1f13b0ab1af5aabd787c879dc16a3cd7
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-11 01:30:06 +0100

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+# Configure the Terraform Provider
+terraform {
+  required_providers {
+    kopicloud = {
+      source  = "https://github.com/KopiCloud-AD-API/terraform-provider-kopicloud-ad-api"
+    }
+  }
+}
+
+# Configure the KopiCloud AD API Provider
+provider "kopicloud" {
+  host = "https://api.kopicloud.local"
+  token   = "Basic b3NKYXu6UZFsdjMyQnVs8DN9"
+}
